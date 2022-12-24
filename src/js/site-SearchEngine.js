3133e05060e5aa08459636a81f81ac9b1ff5c4ef
LaraBiewers <118667773+LaraBiewers@users.noreply.github.com>
2022-12-24 11:13:18 +0100

    internal search 1

---
+++ src/js/site-SearchEngine.js
@@ -0,0 +1,7 @@
+//http://www.javascriptkit.com/script/cut153.shtml
+
+function search_site() {
+
+    document.getElementById("infotext").innerHTML = "Ihre Suchergebnisse werden nun unten aufgelistet!";
+
+}
--- src/js/table-SearchEngine.js
+++ src/js/table-SearchEngine.js
@@ -1,8 +1,8 @@
-/* Refernez: https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/ */
+// Refernez: https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/
 
 function search_veranstaltung() {
 
-    let input = document.getElementById('searchbar').value
+    let input = document.getElementById('searchbar').value;
     input = input.toLowerCase();
     let x = document.getElementsByClassName('veranstaltung');
 
@@ -14,5 +14,5 @@ function search_veranstaltung() {
             x[i].style.display = "table-row";
         }
     }
-    
+
 }
